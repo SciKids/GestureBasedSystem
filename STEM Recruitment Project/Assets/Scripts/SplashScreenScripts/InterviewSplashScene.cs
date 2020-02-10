987e24ee73f8014bd2d974d83393273cdaa3bf82
Sam Earl <sje74@nau.edu>
2020-02-09 21:21:50 -0700

    Added tutorial for interview game

---
+++ STEM Recruitment Project/Assets/Scripts/SplashScreenScripts/InterviewSplashScene.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class InterviewSplashScene : MonoBehaviour
+{
+    public Image pic1, pic2;
+    public string newScene;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        pic1.canvasRenderer.SetAlpha(0.0f);
+        pic2.canvasRenderer.SetAlpha(0.0f);
+        
+        FadeIn(pic1);
+        yield return new WaitForSeconds(8.0f);
+        FadeOut(pic1);
+
+        yield return new WaitForSeconds(2.0f);
+
+        FadeIn(pic2);
+        yield return new WaitForSeconds(8.0f);
+        FadeOut(pic2);
+        yield return new WaitForSeconds(2.0f);
+
+        SceneManager.LoadScene(newScene);
+    }
+    
+    void FadeIn(Image pic)
+    {
+        pic.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+
+    void FadeOut(Image pic)
+    {
+        pic.CrossFadeAlpha(0.0f, 2.5f, false);
+    }
+}
